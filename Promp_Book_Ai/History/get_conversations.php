042df4268e7d9d7257e3499cdcdc4ef15860522a
PTD <115545806+key2212@users.noreply.github.com>
2026-05-05 17:46:41 +0700

    Delete Promp_Book_Ai directory

--- Promp_Book_Ai/History/get_conversations.php
+++
@@ -1,9 +0,0 @@
-<?php
-include __DIR__ . "/connect.php";
-$result = mysqli_query($conn,"SELECT * FROM conversations ORDER BY id DESC");
-$data=[];
-while($row=mysqli_fetch_assoc($result)){
-    $data[]=$row;
-}
-echo json_encode($data);
-?>
